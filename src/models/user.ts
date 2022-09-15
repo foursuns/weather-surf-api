3da2e1b91224300ad427553d79ce737495f84248
Marcio Anthony <anthony.foursuns@gmail.com>
2022-09-15 15:00:47 -0300

    feat: add BaseController, users bad scenarios test cases and update mongoose error types

--- src/models/user.ts
+++ src/models/user.ts
@@ -7,6 +7,10 @@ export interface User {
   password: string;
 }
 
+export enum CUSTOM_VALIDATION {
+  DUPLICATED = 'DUPLICATED',
+}
+
 interface UserModel extends Omit<User, '_id'>, Document {}
 
 const schema = new mongoose.Schema(
@@ -15,7 +19,6 @@ const schema = new mongoose.Schema(
     email: {
       type: String,
       required: true,
-      unique: true,
     },
     password: { type: String, required: true },
   },
@@ -28,6 +31,18 @@ const schema = new mongoose.Schema(
       },
     },
   }
+);
+
+/**
+ * Validates the email and throws a validation error, otherwise it will throw a 500
+ */
+ schema.path('email').validate(
+  async (email: string) => {
+    const emailCount = await mongoose.models.User.countDocuments({ email });
+    return !emailCount;
+  },
+  'already exists in the database.',
+  CUSTOM_VALIDATION.DUPLICATED
 );
 
 export const User: Model<UserModel> = mongoose.model('User', schema);
